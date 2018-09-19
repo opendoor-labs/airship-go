8a04f9496940375d3e1a19cda1b66e70429d3efd
George Li <glusa8@berkeley.edu>
2018-09-18 21:31:08 -0700

    Initial files

---
+++ airship.go
@@ -0,0 +1,115 @@
+package airship
+
+import (
+	"bytes"
+	"time"
+
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+var defaultTimeout = 10 * time.Second
+
+type Client struct {
+	EnvKey         string
+	EdgeURL        string
+	RequestTimeout time.Duration
+}
+
+type AirshipFlag struct {
+	Name   string
+	Client *Client
+}
+
+type ObjectValuesBody struct {
+	Flag   string      `json:"flag"`
+	Entity interface{} `json:"entity"`
+}
+
+type ObjectValues struct {
+	Treatment  string          `json:"treatment"`
+	Payload    json.RawMessage `json:"payload"`
+	IsEligible bool            `json:"isEligible"`
+	IsEnabled  bool            `json:"isEnabled"`
+}
+
+var DefaultClient = &Client{}
+
+// NOTE: this is not thread safe.
+func Configure(c *Client) {
+	DefaultClient = c
+}
+
+func Flag(flagName string) *AirshipFlag {
+	return DefaultClient.Flag(flagName)
+}
+
+func (c *Client) Flag(flagName string) *AirshipFlag {
+	return &AirshipFlag{
+		Name:   flagName,
+		Client: c,
+	}
+}
+
+func (f *AirshipFlag) IsEnabled(entity interface{}) bool {
+	return isEnabled(f, f.Client, entity)
+}
+
+func isEnabled(flag *AirshipFlag, client *Client, entity interface{}) bool {
+	objectValues, err := getObjectValues(flag, client, entity)
+	if err != nil {
+		return false
+	}
+	return objectValues.IsEnabled
+}
+
+func (f *AirshipFlag) GetTreatment(entity interface{}) string {
+	return getTreatment(f, f.Client, entity)
+}
+
+func getTreatment(flag *AirshipFlag, client *Client, entity interface{}) string {
+	objectValues, err := getObjectValues(flag, client, entity)
+	if err != nil {
+		return ""
+	}
+	return objectValues.Treatment
+}
+
+// Pass a pointer as the second argument with the same semantics as json.Unmarshal
+func (f *AirshipFlag) GetPayload(entity interface{}, v interface{}) error {
+	return getPayload(f, f.Client, entity, v)
+}
+
+func getPayload(flag *AirshipFlag, client *Client, entity interface{}, v interface{}) error {
+	objectValues, err := getObjectValues(flag, client, entity)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(objectValues.Payload, v)
+}
+
+func getObjectValues(flag *AirshipFlag, client *Client, entity interface{}) (*ObjectValues, error) {
+	objJson, _ := json.Marshal(&ObjectValuesBody{
+		Flag:   flag.Name,
+		Entity: entity,
+	})
+	requestTimeout := client.RequestTimeout
+	if requestTimeout == 0 {
+		requestTimeout = defaultTimeout
+	}
+	var netClient = &http.Client{
+		Timeout: client.RequestTimeout,
+	}
+	res, err := netClient.Post("http://"+client.EdgeURL+"/v2/object-values/"+client.EnvKey, "application/json", bytes.NewBuffer(objJson))
+	if err != nil {
+		return nil, err
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	var objectValues ObjectValues
+	json.Unmarshal(body, &objectValues)
+	return &objectValues, nil
+}
---
+++ example/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	airship "github.com/airshiphq/airship-go" // we want this folder to just be called airship
+	"os"
+)
+
+// This is our example app's expected entity
+type Entity struct {
+	ID string `json:"id"`
+}
+
+// This is our example app's expected payload
+type Payload struct {
+	Foo string `json:"foo"`
+}
+
+func main() {
+	newInstanceTest()
+	singletonTest()
+}
+
+func newInstanceTest() {
+	airshipInstance := &airship.Client{
+		EnvKey:  os.Getenv("ENV_KEY"),
+		EdgeURL: "localhost:5000",
+	}
+	airshipInstanceBitcoinPay := airshipInstance.Flag("bitcoin-pay")
+	myEntity := &Entity{
+		ID: "1",
+	}
+	fmt.Println(airshipInstanceBitcoinPay.IsEnabled(myEntity))
+
+	fmt.Println(airshipInstanceBitcoinPay.GetTreatment(myEntity))
+
+	var myPayload Payload
+	err := airshipInstanceBitcoinPay.GetPayload(myEntity, &myPayload)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(myPayload.Foo)
+}
+
+func singletonTest() {
+	airship.Configure(&airship.Client{
+		EnvKey:  os.Getenv("ENV_KEY"),
+		EdgeURL: "localhost:5000",
+	})
+	airshipBitcoinPay := airship.Flag("bitcoin-pay")
+	myEntity := &Entity{
+		ID: "2",
+	}
+	fmt.Println(airshipBitcoinPay.IsEnabled(myEntity))
+}
